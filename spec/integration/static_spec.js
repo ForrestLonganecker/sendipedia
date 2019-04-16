01f5e220d2f23ffd166072e95ca0031d6b3c1391
Forrest Longanecker <ForrLong.Tech@gmail.com>
2019-04-15 22:04:46 -0700

    initial view structure implemented

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -5,10 +5,10 @@ const base = 'http://localhost:3000/';
 describe('routes : static', () => {
 
     describe('GET /', () => {
-        it('should return status code 200', (done) => {
+        it('should return status code 200 and have "Welcome to Sendipedia!" in the body', (done) => {
             request.get(base, (err, res, body) => {
                 expect(res.statusCode).toBe(200);
-                expect(body).toContain('Sendipedia');
+                expect(body).toContain('Welcome to Sendipedia');
                 done();
             });
         });
