493ac5e0848cfe8091b6153740554ec996a02251
Forrest Longanecker <ForrLong.Tech@gmail.com>
2019-04-16 08:49:26 -0700

    connected postgres/sequelize database and added logger

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const path = require('path');
 const viewsFolder = path.join(__dirname, '..', 'views');
+const logger = require('morgan');
 
 module.exports = {
     init(app, express){
         app.set('views', viewsFolder);
         app.set('view engine', 'ejs');
+        app.use(logger('dev'));
         app.use(express.static(path.join(__dirname, '..', 'assetss')));
     }
 };
