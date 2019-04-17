512c1222dd33e573da6e6f73d2b195cc9e4389f0
Forrest Longanecker <ForrLong.Tech@gmail.com>
2019-04-17 16:36:03 -0700

    tried to assign css using app.use in app.js

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const app = express();
 const appConfig = require('./config/main-config.js');
 const routeConfig = require('./config/route-config.js');
 
+app.use("/css",express.static(__dirname + "/css"));
+
 appConfig.init(app, express);
 routeConfig.init(app);
 
