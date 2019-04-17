190b6b8d9c0af71e8d2253ec9b6062a03e8200cf
Forrest Longanecker <ForrLong.Tech@gmail.com>
2019-04-17 08:36:42 -0700

    sendgrid is working, issue with users being created with same email

---
+++ src/assets/sendgrid/emails.js
@@ -0,0 +1,16 @@
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+module.exports = {
+  newUserEmail(newUser) {
+    const message = {
+      to: newUser.email,
+      from: { email: 'sendy@sendipedia.com', name: 'Sendy Senderson'},
+      subject: 'Sendipedia email confirmation',
+      text: `Hello, ${newUser.name}, we just wanted to confirm that you signed up with us at Sendipedia! If you are not ${newUser.name}, please let us know!`
+    }
+    sgMail.send(message).then((sent) => {
+      // console.log(sent);
+    })
+  }
+}
