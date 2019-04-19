90b35b2ac128e0fcba7f1647942d4e041d5b1b35
Forrest Longanecker <ForrLong.Tech@gmail.com>
2019-04-19 14:58:31 -0700

    removed console.log

--- src/controllers/wikiController.js
+++ src/controllers/wikiController.js
@@ -4,7 +4,6 @@ module.exports = {
   index(req, res, next){
     wikiQueries.getAllWikis((err, wikis) => {
       if(err){
-        console.log('{CONTROLLER} 1 IF/ERR: ', err);
         res.redirect(500, 'static/index');
       } else {
         res.render('wikis/index', {wikis});
