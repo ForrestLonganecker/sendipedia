ef76cec5ff2b7646d796df0a7a4256bfe3a4ab18
Forrest Longanecker <ForrLong.Tech@gmail.com>
2019-05-01 17:17:11 -0700

    small add

--- src/db/queries.collaborators.js
+++ src/db/queries.collaborators.js
@@ -3,5 +3,10 @@ const Authorizer = require('../policies/collaborator');
 const markdown = require('markdown').markdown;
 
 module.exports = {
-  
+  getAllCollaborators(wikiId, callback){
+    return Collaborator.findAll({where: {wikiId: wikiId}})
+    .then((wikis) => {
+      callback
+    })
+  }
 }
